18cb2a1fbde5a40ef232e27a4b1c5ce821f35ce1
- <->
2025-09-24 06:01:09 +0200

    integrate for using setup key

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+SETUP_KEY_FILE="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/.setup_key"
+
+[[ ! -f "$SETUP_KEY_FILE" ]] && echo "File '.setup_key' is missing! create it, and write netbird setup key in it!" && exit 1
+
 podman run -it --rm \
     --cap-add NET_ADMIN \
     --cap-add SYS_ADMIN \
     --cap-add NET_RAW \
     --device /dev/net/tun \
-    ghcr.io/daniele47/neovim bash
+    -e NB_SETUP_KEY="$(cat "$SETUP_KEY_FILE")" \
+    ghcr.io/daniele47/netbird bash
