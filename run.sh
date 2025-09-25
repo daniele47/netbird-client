27128440dbf4622990e26992401d668a013d1c49
- <->
2025-09-25 19:01:44 +0200

    add .bash_init to allow user only customizations inside the container (keep state, without using volumes)

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 SETUP_KEY_FILE="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/.setup_key"
+BASHINIT_FILE="$(dirname "$(realpath "${BASH_SOURCE[0]}")")/.bash_init"
 
 [[ ! -f "$SETUP_KEY_FILE" ]] && echo "File '.setup_key' is missing! create it, and write netbird setup key in it!" && exit 1
+
+# mount bash init if it exists
+bash_init=()
+[[ -f "$BASHINIT_FILE" ]] && bash_init+=(--security-opt label=type:container_runtime_t -v "$BASHINIT_FILE:/root/.bash_init")
 
 podman run -it --rm \
     --cap-add NET_ADMIN \
@@ -10,4 +15,5 @@ podman run -it --rm \
     --cap-add NET_RAW \
     --device /dev/net/tun \
     -e NB_SETUP_KEY="$(cat "$SETUP_KEY_FILE")" \
+    "${bash_init[@]}" \
     ghcr.io/daniele47/netbird bash
